6c3965fdf60e229e48dc576fd7227197e90c8627
ChathuraSamarakoon <chathurasamarakoon849495@gmail.com>
2026-05-31 13:51:59 +0530

    chore: initial spring boot project setup with fudo_db connection

---
+++ src/main/java/fudo_backend/FudoBackendApplication.java
@@ -0,0 +1,13 @@
+package fudo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FudoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FudoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fudo_backend/FudoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fudo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FudoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
